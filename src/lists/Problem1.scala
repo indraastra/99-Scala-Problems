8c3b5bc44b9b7379614cb429845d16abd7fd2142
Vishal Talwar <rawlatv@gmail.com>
2011-09-01 01:21:06 -0500

    Finished problem 1 and added simple tests.

--- src/lists/Problem1.scala
+++ src/lists/Problem1.scala
@@ -1,12 +1,20 @@
 package lists
 
 /**
- * @author rawlatv@gmail.com (Vishal Talwar)
+ * Finds the last element of a list.
  *
+ * @author rawlatv@gmail.com (Vishal Talwar)
  */
 object Problem1 {
 
-  def main(args: Array[String]) {
-    println("Hello World")
+  def last[A]( l : List[A] ) = {
+    l.last
+  }
+
+  def main( args : Array[String] ) {
+    assert( last( List( 1 ) ) == 1, "integer list" )
+    assert( last( List( 1.1, 2.2, 3.3 ) ) == 3.3, "float list" )
+    assert( last( List( "foo", "bar" ) ) == "bar", "string list" )
+    println( "all passed!" )
   }
 }
