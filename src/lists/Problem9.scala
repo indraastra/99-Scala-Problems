714903e5b27116d2ac1c06477e314be93651a6e0
Vishal Talwar <rawlatv@gmail.com>
2011-09-25 19:17:01 -0700

    finished problem 9

---
+++ src/lists/Problem9.scala
@@ -0,0 +1,27 @@
+package lists
+
+/**
+ * Pack consecutive duplicates of list elements into sublists.
+ *
+ * @author rawlatv@gmail.com (Vishal Talwar)
+ */
+object Problem9 {
+
+  def pack[A]( l: List[A] ): List[List[A]] = ( l :\ List[List[A]]() ) {
+    ( x, xs ) =>
+      if ( !xs.isEmpty && x == xs.head.head )
+        ( x :: xs.head ) :: xs.tail
+      else
+        List( x ) :: xs
+  }
+
+  def main( args: Array[String] ): Unit = {
+    assert(
+      pack( List( 'a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e ) ) ==
+        List( List( 'a, 'a, 'a, 'a ), List( 'b ), List( 'c, 'c ), List( 'a, 'a ), List( 'd ),
+          List( 'e, 'e, 'e, 'e ) ) )
+    println( "all passed!" )
+
+  }
+
+}
