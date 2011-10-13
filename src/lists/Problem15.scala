566dec374b14d478e121a1557a42d443a8ee2dde
Vishal Talwar <rawlatv@gmail.com>
2011-10-12 18:21:36 -0700

    finished problem 15

---
+++ src/lists/Problem15.scala
@@ -0,0 +1,26 @@
+package lists
+
+/**
+ * Duplicate the elements of a list a given number of times.
+ *
+ * @author rawlatv@gmail.com (Vishal Talwar)
+ */
+object Problem15 {
+
+  def duplicateN_1[A]( n: Int, l: List[A] ): List[A] = {
+    Problem12.decode( l.map( x => ( n, x ) ) )
+  }
+
+  def duplicateN_2[A]( n: Int, l: List[A] ): List[A] = {
+    l.flatMap { x => Problem12.repeat( x, n ) }
+  }
+
+  def main( args: Array[String] ): Unit = {
+    val solutions = List( duplicateN_1[Symbol] _, duplicateN_2[Symbol] _ )
+    for ( fn <- solutions ) {
+      assert( fn( 3, List( 'a, 'b, 'c, 'c, 'd ) ) ==
+        List( 'a, 'a, 'a, 'b, 'b, 'b, 'c, 'c, 'c, 'c, 'c, 'c, 'd, 'd, 'd ) )
+      println( "all passed!" )
+    }
+  }
+}
